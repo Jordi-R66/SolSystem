5d78f8bb89d006148f41f1bb9bb56eb905c89c0b
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-01-01 02:06:38 +0100

    Moved types definition inside Common.h

--- libs/FileParser.h
+++ libs/FileParser.h
@@ -2,22 +2,6 @@
 
 #include "CONVERSIONS.h"
 
-#define NAME_LENGTH 26
-
-#define BODY_ID_LENGTH 5
-#define ID_BITS 8
-
-#define BODY_MASS_LENGTH 16
-
-#define SMA_LENGTH 31
-#define ECC_LENGTH 16
-#define INCLINATION_LENGTH 11
-#define LNG_LENGTH 11
-
-#define FIELDS 10
-
-#define LINE_LENGTH (NAME_LENGTH + 2 * BODY_ID_LENGTH + BODY_MASS_LENGTH + SMA_LENGTH + ECC_LENGTH + INCLINATION_LENGTH + 3 * LNG_LENGTH + 1)
-
 /*
 #define NORAD_ID_LENGTH 6
 #define YR_LENGTH 3
@@ -39,43 +23,6 @@
 
 #define COSPAR_LENGTH (YR_LENGTH + LAUNCH_NB_LENGTH + LAUNCH_PART_LENGTH - 2)
 */
-
-typedef int8_t id_t;
-
-typedef struct SysConf {
-	string SysName[NAME_LENGTH];
-	float MassScale;
-	float DistScale;
-	float Epoch_TT;
-	bool HeaderLine;
-} SysConf;
-
-typedef struct Body {
-	SysConf* sysConf;
-	id_t BodyId;
-	id_t ParentId;
-
-	string BodyName[NAME_LENGTH];
-
-	bool hasParent;
-
-	float BodyMass;
-
-	float SemiMajorAxis;
-	float Eccentricity;
-	float Inclination;
-	float AscNodeLong;
-	float PeriLong;
-	float MeanLong;
-
-	float PeriArg;
-	float MeanAnomaly;
-	float MeanMotion;
-
-	float x, y, z;
-	float v_x, v_y, v_z;
-
-} Body;
 
 void parse_line(string* line, Body* body);
 
