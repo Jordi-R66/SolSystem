0be552bcb8098e9e8753c0ab2ebbe1b3dd758f02
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-01 23:26:46 +0200

    Correction du nom de l'exécutable dans compile.sh de "SolSytem" à "SolSystem".

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ OUTPUT_DIR="output"
 
 mkdir -p $OUTPUT_DIR
 
-EXEC="SolSytem"
+EXEC="SolSystem"
 
 ARGS="-Wall -O3 -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
 ARGS="-Wall -O3 -g -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
