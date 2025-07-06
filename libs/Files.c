62da311e4f8c3321926580b418c19c7962f51cac
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-06 16:05:08 +0200

    Renommer la fonction ExportBodies en ExportSimplifiedBodies et simplifier l'exportation des corps simplifiés.

--- libs/Files.c
+++ libs/Files.c
@@ -1,32 +1,10 @@
 #include "FileParser.h"
 #include "Files.h"
 
-void ExportBodies(BodyFile* bodyFile, SysConf* sysConf) {
+void ExportSimplifiedBodies(SimplifiedBody* bodies, size_t numberOfBodies, SysConf* sysConf) {
 	FILE* fp = fopen(sysConf->SysName, "w");
 
-	SimplifiedBody* temp = (SimplifiedBody*)calloc(bodyFile->numberOfBodies, SIMPLIFIED_BODY_SIZE);
-
-	for (size_t i = 0; i < bodyFile->numberOfBodies; i++) {
-		Body bodyToCopy = bodyFile->bodies[i];
-
-		SimplifiedBody* bodyToWrite = temp + i;
-
-		bodyToWrite->bodyId = bodyToCopy.BodyId;
-		memcpy(bodyToWrite->BodyName, bodyToCopy.BodyName, NAME_LENGTH);
-
-		bodyToWrite->hasParent = bodyToCopy.hasParent;
-		bodyToWrite->parentId = bodyToCopy.ParentId;
-
-		bodyToWrite->Mass = bodyToCopy.BodyMass;
-		bodyToWrite->SMA = bodyToCopy.SemiMajorAxis;
-		bodyToWrite->Eccentricity = bodyToCopy.Eccentricity;
-		bodyToWrite->Inclination = bodyToCopy.Inclination;
-		bodyToWrite->AscNode = bodyToCopy.AscNodeLong;
-		bodyToWrite->PeLNG = bodyToCopy.PeriLong;
-		bodyToWrite->MeanLNG = bodyToCopy.MeanLong;
-	}
-
-	fwrite((void*)temp, SIMPLIFIED_BODY_SIZE, bodyFile->numberOfBodies, fp);
+	fwrite((void*)bodies, SIMPLIFIED_BODY_SIZE, numberOfBodies, fp);
 
 	fclose(fp);
 }
