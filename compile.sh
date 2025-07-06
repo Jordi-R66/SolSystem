47dc824a09e25fbc8d7f23754c5f7f7cb419694f
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-06 15:58:25 +0200

    Correction des options de compilation dans compile.sh pour inclure -mlong-double-128 et ajuster le niveau d'optimisation.

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ mkdir -p $OUTPUT_DIR
 
 EXEC="SolSystem"
 
-ARGS="-Wall -O3 -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
-ARGS="-Wall -O3 -g -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
+ARGS="-Wall -O3 -mlong-double-128 -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
+ARGS="-Wall -O0 -mlong-double-128 -g -std=gnu17 -masm=intel -m64 -march=x86-64 -flto"
 
 $COMPILER $ARGS $SOURCES -o $OUTPUT_DIR/$EXEC.out -lm
