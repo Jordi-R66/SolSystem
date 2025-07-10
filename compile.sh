70f4a76daeecc4abf91a30beacd412c6954b4977
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-10 16:59:11 +0200

    Mise à jour des options de compilation pour activer les avertissements et optimiser le code.

--- compile.sh
+++ compile.sh
@@ -10,7 +10,7 @@ mkdir -p $OUTPUT_DIR
 
 EXEC="SolSystem"
 
-ARGS="-Wall -O3 -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
-ARGS="-Wall -O0 -g -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
+ARGS="-Wall -O3 -s -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
+#ARGS="-Wall -O0 -g -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
 
 $COMPILER $ARGS $SOURCES -o $OUTPUT_DIR/$EXEC.out -lm
