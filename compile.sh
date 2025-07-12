8f32a3e0d2ea0bca307b1c03ad84a4e2d6791f35
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2025-07-12 21:44:17 +0200

    Ajout de la génération du code assembleur lors de la compilation

--- compile.sh
+++ compile.sh
@@ -7,10 +7,15 @@ SOURCES="libs/OrbMaths.c libs/FileParser.c libs/CONVERSIONS.c libs/Algos.c libs/
 OUTPUT_DIR="output"
 
 mkdir -p $OUTPUT_DIR
+mkdir -p asm
 
 EXEC="SolSystem"
 
-ARGS="-Wall -O3 -s -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
-#ARGS="-Wall -O0 -g -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
+#ARGS="-Wall -O3 -s -std=gnu23 -masm=intel -m64 -march=x86-64 -flto"
+ARGS="-Wall -O0 -g -std=gnu23 -masm=intel -m64 -march=x86-64"
 
 $COMPILER $ARGS $SOURCES -o $OUTPUT_DIR/$EXEC.out -lm
+
+$COMPILER $ARGS $SOURCES -lm -S
+
+mv *.s asm/
